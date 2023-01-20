087d9affd43e1404fe0694394fa262e095a47068
Thi Thuy Oanh Nguyen <thuyoanh199930@gmail.com>
2023-01-20 17:14:39 +0000

    how to find the description of a cat based on breed name and print out the error message when the breed name is empty

---
+++ breedFetcher.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+//codes to test out if the request library has been successfully required:
+// request('http://www.google.com', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+// });
+
+//get the description from API server based on breed name:
+const BreedName = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${BreedName}`;
+request(url, function (error, response, body) {
+  const data = JSON.parse(body);
+  if (!data[0]) {
+    console.log('breed not found');
+  } else {
+    console.log(data[0].description);
+  }
+});
+
+// const FindBreadByName = function(name, callback) {
+//   const url   = `https://api.thecatapi.com/v1/breeds/search?q=${name}`;
+//   request (url, (error, response, body) => {
+//     const data = JSON.parse(body);
+//     let errorMes = error;
+//     let description = data[0].description;
+//     return callback(errorMes, description);
+//   })
+// };
